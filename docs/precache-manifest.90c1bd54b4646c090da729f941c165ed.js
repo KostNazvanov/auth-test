69f0ff492e2ac73037b398061e7ab60ac783227f
Kostia Nazvanov <nazvanov.kst@gmail.com>
2020-04-07 15:59:18 +0300

    Push build for GitHub Pages

---
+++ docs/precache-manifest.90c1bd54b4646c090da729f941c165ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e53a7ee0093aa84961ba1fdcda51b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d77057d4d2aefe02aa1",
+    "url": "/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "54d9bc25d50e2595c7ba",
+    "url": "/static/css/main.aec0262f.chunk.css"
+  },
+  {
+    "revision": "6d77057d4d2aefe02aa1",
+    "url": "/static/js/2.cd227e59.chunk.js"
+  },
+  {
+    "revision": "1e2254fda7d38af03b0904d147bcf897",
+    "url": "/static/js/2.cd227e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d9bc25d50e2595c7ba",
+    "url": "/static/js/main.6ff7833b.chunk.js"
+  },
+  {
+    "revision": "b630f06cd5eaa37d5b6f",
+    "url": "/static/js/runtime-main.5c11e176.js"
+  }
+]);
